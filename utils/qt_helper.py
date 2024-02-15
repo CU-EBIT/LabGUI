465dd518e4f1eb88a0f03726c959511382dfca4b
Patrick Johnson <40612672+prjohns@users.noreply.github.com>
2024-02-15 09:56:27 -0500

    Update qt_helper.py

--- utils/qt_helper.py
+++ utils/qt_helper.py
@@ -1,13 +1,13 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QComboBox, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout,\
-        QHBoxLayout, QWidget, QFrame, QPushButton, QLineEdit, QFileDialog, QTableWidget,\
-        QMainWindow, QMenu, QApplication
-from PyQt6 import QtCore, QtWidgets, QtGui
-from PyQt6.QtGui import QAction, QIcon
-
-# from PyQt5.QtCore import Qt
-# from PyQt5.QtWidgets import QComboBox, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout,\
+# from PyQt6.QtCore import Qt
+# from PyQt6.QtWidgets import QComboBox, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout,\
 #         QHBoxLayout, QWidget, QFrame, QPushButton, QLineEdit, QFileDialog, QTableWidget,\
-#         QMainWindow, QMenu, QAction, QApplication
-# from PyQt5 import QtCore, QtWidgets, QtGui
-# from PyQt5.QtGui import QIcon
+#         QMainWindow, QMenu, QApplication
+# from PyQt6 import QtCore, QtWidgets, QtGui
+# from PyQt6.QtGui import QAction, QIcon
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QComboBox, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout,\
+        QHBoxLayout, QWidget, QFrame, QPushButton, QLineEdit, QFileDialog, QTableWidget,\
+        QMainWindow, QMenu, QAction, QApplication
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtGui import QIcon
