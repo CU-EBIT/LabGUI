63f055589cfb9a857c1f51f27152689c14045d34
Patrick Johnson <40612672+prjohns@users.noreply.github.com>
2024-03-28 09:05:21 -0400

    Update qt_helper.py

--- lab_gui/utils/qt_helper.py
+++ lab_gui/utils/qt_helper.py
@@ -1,15 +1,17 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QComboBox, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout,\
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QComboBox, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout,\
         QHBoxLayout, QWidget, QFrame, QPushButton, QLineEdit, QFileDialog, QTableWidget,\
         QMainWindow, QMenu, QApplication, QLayout, QGridLayout
-from PyQt6 import QtCore, QtWidgets, QtGui
-from PyQt6.QtGui import QAction, QIcon
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtGui import QAction, QIcon
 
 Key_Up = QtCore.Qt.Key.Key_Up
 Key_Down = QtCore.Qt.Key.Key_Down
 Key_Enter = QtCore.Qt.Key.Key_Enter
 
 TableNoEdit = QtWidgets.QTableWidget.EditTrigger.NoEditTriggers
+# PySide6 has this named differently it seems
+QtCore.pyqtSignal = QtCore.Signal
 
 # from PyQt5.QtCore import Qt
 # from PyQt5.QtWidgets import QComboBox, QLabel, QCheckBox, QHBoxLayout, QVBoxLayout,\
